3bd2b852bccc88264fd2f3756cece6b6cb4adc7f
Dániel Sipos <siposdani87@hotmail.com>
2021-04-01 20:35:23 +0200

    Create prototype of polygon editor

--- App.tsx
+++ App.tsx
@@ -1,12 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import MapView, { MapEvent } from 'react-native-maps';
+import { area1, area2 } from './area';
+import PolygonEditor, { MapPolygonExtendedProps, PolygonEditorRef } from './src/PolygonEditor';
+
+function getRandomNumber(min: number, max: number): number {
+  return (Math.random() * max) + min;
+}
+
+function getRandomColors(): string[] {
+  const red = Math.floor(getRandomNumber(0, 255));
+  const green = Math.floor(getRandomNumber(0, 255));
+  const blue = Math.floor(getRandomNumber(0, 255));
+  return [`rgb(${red}, ${green}, ${blue})`, `rgba(${red}, ${green}, ${blue}, 0.2)`];
+}
+
+const [strokeColor1, fillColor1] = getRandomColors();
+const polygon1 = {
+  coordinates: area1,
+  strokeWidth: 2,
+  strokeColor: strokeColor1,
+  fillColor: fillColor1,
+};
+
+const [strokeColor2, fillColor2] = getRandomColors();
+const polygon2 = {
+  coordinates: area2,
+  strokeWidth: 2,
+  strokeColor: strokeColor2,
+  fillColor: fillColor2,
+};
+
+const [strokeColor3, fillColor3] = getRandomColors();
+const newPolygon = {
+  coordinates: [],
+  strokeWidth: 2,
+  strokeColor: strokeColor3,
+  fillColor: fillColor3,
+};
 
 export default function App() {
+  const [polygons, setPolygons] = useState([]);
+
+  const mapRef = useRef(null);
+  const polygonEditorRef = useRef<PolygonEditorRef>(null);
+
+  useEffect(() => {
+    setPolygons([polygon1, polygon2]);
+  }, []);
+
+  useEffect(() => {
+    mapRef.current?.fitToCoordinates(polygons[0]?.coordinates, {
+      edgePadding: { top: 40, right: 40, bottom: 40, left: 40 },
+      animated: true,
+    });
+  }, [mapRef.current]);
+
+  useEffect(() => {
+    console.log(polygons.length);
+  }, [polygons]);
+
+  function clickOnMap({ nativeEvent: { coordinate } }: MapEvent) {
+    polygonEditorRef.current?.setCoordinate(coordinate);
+  }
+
+  function createNewPolygon() {
+    polygonEditorRef.current?.startPolygon();
+  }
+
+  function onPolygonChange(index: number, polygon: MapPolygonExtendedProps) {
+    console.log('onPolygonChange');
+    const polygonsClone = [...polygons];
+    polygonsClone[index] = polygon;
+    setPolygons(polygonsClone);
+  }
+
+  function onPolygonCreate(polygon: MapPolygonExtendedProps) {
+    console.log('onPolygonCreate');
+    const polygonsClone = [...polygons, polygon];
+    setPolygons(polygonsClone);
+  }
+
+  function onPolygonRemove(index: number) {
+    console.log('onPolygonRemove');
+    const polygonsClone = [...polygons];
+    polygonsClone.splice(index, 1);
+    setPolygons(polygonsClone);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto'/>
+      <MapView ref={mapRef} onPress={clickOnMap} style={styles.mapContainer}>
+        <PolygonEditor ref={polygonEditorRef} newPolygon={newPolygon} polygons={polygons} onPolygonChange={onPolygonChange} onPolygonCreate={onPolygonCreate} onPolygonRemove={onPolygonRemove} />
+      </MapView>
+      <View style={styles.actionsContaiener}>
+        <Button onPress={createNewPolygon} title='New polygon' />
+      </View>
     </View>
   );
 }
@@ -17,5 +108,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  actionsContaiener: {
+    position: 'absolute',
+    bottom: 50,
+    left: 10,
   },
 });
