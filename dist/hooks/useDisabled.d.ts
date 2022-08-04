23daa91a25d439c2922582e53b42827087b9e045
Dániel Sipos <siposdani87@hotmail.com>
2022-08-04 13:10:43 +0200

    Upgrade expo to version 46

--- dist/hooks/useDisabled.d.ts
+++ dist/hooks/useDisabled.d.ts
@@ -1,1 +1,1 @@
-export declare const useDisabled: (onDisabled: () => void, oldDisabled?: boolean | undefined) => boolean;
+export declare const useDisabled: (onDisabled: () => void, oldDisabled?: boolean) => boolean;
