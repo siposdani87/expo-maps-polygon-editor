2789d6896f2c6f7ecfa2dd498e2cc04f112ca683
Dániel Sipos <siposdani87@hotmail.com>
2022-09-18 22:09:49 +0200

    Upgrade packages

--- example/start.sh
+++ example/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# sudo npm i -g expo-cli expo-optimize
-npm i
 rm -rf node_modules/@siposdani87
 mkdir -p node_modules/@siposdani87/expo-maps-polygon-editor
 cp -R ../dist node_modules/@siposdani87/expo-maps-polygon-editor
 cp -R ../package.json node_modules/@siposdani87/expo-maps-polygon-editor
 npm run tsc-test 
-expo start -c
+npm run start
