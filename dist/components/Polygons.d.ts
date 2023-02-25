56cc5b1f7f4a5db42e68816bc3d70f72d80b5cf9
Dániel Sipos <siposdani87@hotmail.com>
2023-02-25 20:36:34 +0100

    Create build

--- dist/components/Polygons.d.ts
+++ dist/components/Polygons.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 import { MapPolygonExtendedProps } from '../lib/types';
-export declare type PolygonPressEvent = any;
+export type PolygonPressEvent = any;
 export default function Polygons(props: {
     polygons: MapPolygonExtendedProps[];
     onPolygonClick: (index: number, polygon: MapPolygonExtendedProps) => (e: PolygonPressEvent) => void;
--- dist/lib/types.d.ts
+++ dist/lib/types.d.ts
@@ -1,12 +1,12 @@
 import { LatLng, MapPolygonProps } from 'react-native-maps';
-export declare type PolygonKey = string | number | null;
-export declare type PolygonEditorRef = {
+export type PolygonKey = string | number | null;
+export type PolygonEditorRef = {
     setCoordinate: (coordinate: LatLng) => void;
     startPolygon: () => void;
     selectPolygonByKey: (key: PolygonKey) => void;
     selectPolygonByIndex: (index: number) => void;
     resetAll: () => void;
 };
-export declare type MapPolygonExtendedProps = MapPolygonProps & {
+export type MapPolygonExtendedProps = MapPolygonProps & {
     key: PolygonKey;
 };
